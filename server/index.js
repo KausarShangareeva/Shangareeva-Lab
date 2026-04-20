2c6ddd5cf8b4f33624f669768b10f92db8facf5d
kausyar-nordic-design-AB <kausyar@swedishnordicdesign.com>
2026-04-20 17:12:41 +0200

    Update CORS to hadiyaandlab.netlify.app

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://shangareevalab.netlify.app",
+      "https://hadiyaandlab.netlify.app",
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     methods: ["GET", "POST", "PUT", "DELETE"],
